c9f75503581f82a58327f9c50ff71cc81906be34
Thomas Thiry <thomas.thiry@gmail.com>
2015-12-28 17:25:16 +0100

    Initial application with basic hardcoded list

---
+++ PayMeBack/gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require('gulp');
+
+var paths = {
+    bower: "./bower_components/",
+    scripts: "./www/libraries/",
+    css: "./www/css/",
+    fonts: "./www/fonts/"
+};
+
+gulp.task("copy_libs", function () {
+    gulp.src(paths.bower + 'ionic/release/css/ionic.css').pipe(gulp.dest(paths.css));
+    gulp.src(paths.bower + 'ionic/release/fonts/ionicons.ttf').pipe(gulp.dest(paths.fonts));
+    gulp.src(paths.bower + 'ionic/release/js/ionic.bundle.js').pipe(gulp.dest(paths.scripts));
+});
---
+++ PayMeBack/www/scripts/app.js
@@ -0,0 +1,38 @@
+﻿var app = angular.module('PayMeBack', ['ionic']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('splitList', {
+            url: '/splits',
+            templateUrl: 'views/splitList.html',
+            controller: 'splitList'
+        })
+        .state('splitView', {
+            url: '/splits/:splitId',
+            templateUrl: 'views/splitView.html',
+            controller: 'splitView'
+        });
+
+    $urlRouterProvider.otherwise('/splits');
+});
+
+app.controller('splitList', ['$scope', 'splitService', function ($scope, splitService) {
+    $scope.splits = splitService.list();
+}]);
+
+app.controller('splitView', ['$scope', '$stateParams', 'splitService', function ($scope, $stateParams, splitService) {
+    $scope.split = splitService.get($stateParams.splitId);
+}]);
+
+app.factory('splitService', function() {
+    var _splits = [{ id: 1, date: 'yesterday' }, { id: 2, date: 'today' }, { id: 3, date: 'Sat 25 Oct 2015' }];
+    return {
+        list: function () {
+            return _splits;
+        },
+        get: function (splitId) {
+            var matchingSplits = _splits.filter(function (value, index, ar) { return value.id == splitId });
+            return matchingSplits.length > 0 ? matchingSplits[0] : null;
+        }
+    };
+});
