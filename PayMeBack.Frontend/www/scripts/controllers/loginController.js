a2674832841dc501a2b34a3d46c0af4af61195ab
Thomas Thiry <thomas.thiry@gmail.com>
2016-06-05 21:42:17 +0200

    Hidden back button after login

--- PayMeBack.Frontend/www/scripts/controllers/loginController.js
+++ PayMeBack.Frontend/www/scripts/controllers/loginController.js
@@ -1,10 +1,13 @@
-﻿angular.module('PayMeBack').controller('LoginController', function ($scope, $state, $ionicPopup, authService) {
+﻿angular.module('PayMeBack').controller('LoginController', function ($scope, $state, $ionicPopup, $ionicHistory, authService) {
 
     $scope.data = {};
 
     $scope.login = function (data) {
         authService.login(data.username, data.password).then(
             function (authenticated) {
+                $ionicHistory.nextViewOptions({
+                    disableBack: true
+                });
                 $state.go('splitList', {}, { reload: true });
                 $scope.setCurrentUsername(data.username);
             },
